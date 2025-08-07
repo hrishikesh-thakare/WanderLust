8e39a51d7963fa84be1a3424b465529dff900fef
Hrishikesh Thakare <hrishikeshthakare0809@gmail.com>
2025-08-07 22:26:12 +0530

    Added CSS

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, '/public')));
 
 main()
     .then(() => {console.log('Connected to MongoDB');})
@@ -87,5 +90,5 @@ app.delete('/listings/:id', async (req, res) => {
 
 
 app.listen(8080, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on http://localhost:8080');
 });
